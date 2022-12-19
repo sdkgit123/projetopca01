f1dc6c9f81dda40920ce74f2e469708622d87e82
Beatriz Guedes <120344463+sdkgit123@users.noreply.github.com>
2022-12-18 22:25:35 -0400

    Add files via upload

---
+++ raio_circulo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main ()
+{
+float raio;
+printf("Descubra a area de um circulo a partir do seu raio!\nDigite o raio do circulo:\n");
+scanf("%f", &raio);
+float area;
+area = 3.14 * (raio * raio);
+printf("A area de uma circulo de raio %1.2f e %1.2f\n", raio, area);
+return 0;
+}
