8c5ee99c9c19b8e367aa1646271a7e37170729c8
Beatriz Guedes <120344463+sdkgit123@users.noreply.github.com>
2022-12-22 13:51:25 -0400

    Add files via upload

---
+++ 20222220006.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+	int i;
+	int cont;
+	for(i=1; i<11; i++){
+		for(cont=1; cont<11; cont++){
+	printf(" %d ", i*cont);
+}
+printf("\n");
+}
+	return 0;
+}
